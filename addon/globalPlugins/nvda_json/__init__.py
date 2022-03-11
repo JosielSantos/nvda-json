eff14260cf12d5acc1449513b5f672107a9a1963
Josiel Santos <josiel.lkp@gmail.com>
2022-03-10 23:29:29 -0300

    Refact: Method and variable renaming, method extraction

--- addon/globalPlugins/nvda_json/__init__.py
+++ addon/globalPlugins/nvda_json/__init__.py
@@ -6,36 +6,37 @@ import ui
 import json
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-    def _get_selected_text(self):
-        # Code adapted from script_reportCurrentSelection on the NVDA source code
-
-        obj=api.getFocusObject()
-        treeInterceptor=obj.treeInterceptor
-        if isinstance(treeInterceptor,treeInterceptorHandler.DocumentTreeInterceptor) and not treeInterceptor.passThrough:
-            obj=treeInterceptor
-
+    def script_format_json_from_selected_text_or_clipboard(self, gesture):
+        text = self.__get_text()
         try:
-            info=obj.makeTextInfo(textInfos.POSITION_SELECTION)
-        except (RuntimeError, NotImplementedError):
-            info=None
+            parsed_json = json.loads(text)
+        except json.decoder.JSONDecodeError as error:
+            ui.message('Invalid JSON: %s' % error)
+        formatted_json = self.__format_json(parsed_json)
+        ui.browseableMessage(formatted_json, 'Formatted JSON', False)
 
+    def __get_text(self):
+        return self.__get_selected_text() or api.getClipData()
+
+    def __get_selected_text(self):
+        # Code adapted from script_reportCurrentSelection on the NVDA source code
+        focused_object = api.getFocusObject()
+        treeInterceptor = focused_object.treeInterceptor
+        if isinstance(treeInterceptor, treeInterceptorHandler.DocumentTreeInterceptor) and not treeInterceptor.passThrough:
+            focused_object = treeInterceptor
+        try:
+            info = focused_object.makeTextInfo(textInfos.POSITION_SELECTION)
+        except (RuntimeError, NotImplementedError):
+            info = None
         if not info or info.isCollapsed:
-            return ""
+            return ''
         else:
             return info.text
 
-    def script_formatJsonFromSelectedTextOrClipboard(self, gesture):
-        selected_text = self._get_selected_text()
-        text = selected_text or api.getClipData()
+    def __format_json(self, parsed_json):
+        return json.dumps(parsed_json, indent=4, sort_keys=True)
 
-        try:
-            parsedJson = json.loads(text)
-        except json.decoder.JSONDecodeError as error:
-            ui.message('Invalid JSON: %s' % error)
-
-        formattedJson = json.dumps(parsedJson, indent=4, sort_keys=True)
-        ui.browseableMessage(formattedJson, 'Formatted JSON', False)
 
     __gestures = {
-        "kb:nvda+j": "formatJsonFromSelectedTextOrClipboard",
+        'kb:nvda+j': 'format_json_from_selected_text_or_clipboard',
     }
