501d29ea37a6537d2f3ea896bc4d2571174153d5
Josiel Santos <josiel.lkp@gmail.com>
2024-11-15 11:16:11 -0300

    feat: Implemented JQ parsing

--- addon/globalPlugins/nvda_json/json_query_dialog.py
+++ addon/globalPlugins/nvda_json/json_query_dialog.py
@@ -5,6 +5,7 @@ import ui
 
 import jsonpath_ng
 import jsonpath_ng.ext
+import jq
 import wx
 
 from .abstract import JsonManipulatorDialog
@@ -12,7 +13,7 @@ from .abstract import JsonManipulatorDialog
 class JsonQueryDialog(JsonManipulatorDialog):
     query_engines = {
         'jq': {
-            'edit_label': 'JQ Expression',
+            'edit_label': 'JQ Program',
             'handler': 'parse_jq',
         },
         'jsonpath': {
@@ -30,20 +31,24 @@ class JsonQueryDialog(JsonManipulatorDialog):
 
     def manipulate(self, event):
         expression = self.manipulation_expression.GetValue().strip()
-        matches = getattr(self, self.query_engine['handler'])(expression)
+        data = json.loads(self.parse_text(self.text, self.multi))
+        matches = getattr(self, self.query_engine['handler'])(data, expression)
         if matches:
             matches = matches[0] if (len(matches) == 1 and isinstance(matches[0], list)) else matches
             self.set_output(self.format_json(matches))
         else:
             ui.message('No matches found')
 
-    def parse_jsonpath(self, expression):
+    def parse_jsonpath(self, data, expression):
         try:
             jsonpath = jsonpath_ng.ext.parse(expression)
-            data = json.loads(self.parse_text(self.text, self.multi))
             return [match.value for match in jsonpath.find(data)]
         except (jsonpath_ng.exceptions.JsonPathLexerError, jsonpath_ng.exceptions.JsonPathParserError) as e:
             ui.message(f'JSONPath Expression Error: {str(e)}')
 
-    def parse_jq(self, expression):
-        ui.message('Not implemented')
+    def parse_jq(self, data, expression):
+        try:
+            program = jq.compile(expression)
+            return program.input(data).all()
+        except Exception as e:
+            ui.message(f'JQ Expression Error: {str(e)}')
